e96177d85fbd0d9b82e0ab04e0fa16bb4da0cae9
amiralles <amiralles.net@gmail.com>
2016-07-29 17:58:10 -0300

    add ref System.Data

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+clear;
 #lib
-mcs src/core/*.cs  -t:library -out:bin/Abacus.dll
+mcs src/core/*.cs  -t:library -out:bin/Abacus.dll -r:System.Data
 
 # tests
-mcs src/test/*.cs -r:lib/Contest.Core.dll -r:lib/unbinder.dll -r:bin/Abacus.dll -t:library -out:bin/Abacus.Test.dll
+mcs src/test/*.cs -r:lib/Contest.Core.dll \
+	 -r:lib/unbinder.dll -r:System.Data -r:bin/Abacus.dll \
+	 -t:library -out:bin/Abacus.Test.dll
--- dbgbuild.sh
+++ dbgbuild.sh
@@ -1,5 +1,12 @@
+clear;
+
 #lib
-mcs src/core/*.cs  -t:library -out:bin/Abacus.dll -define:DEBUG
+mcs src/core/*.cs  -t:library -out:bin/Abacus.dll\
+   -r:System.Data\
+   -define:DEBUG
 
 # tests
-mcs src/test/*.cs -r:lib/unbinder.dll -r:lib/Contest.Core.dll -r:bin/Abacus.dll -t:library -out:bin/Abacus.Test.dll -define:DEBUG
+mcs src/test/*.cs -r:lib/unbinder.dll -r:lib/Contest.Core.dll\
+   	-r:bin/Abacus.dll -r:System.Data\
+   	-t:library -out:bin/Abacus.Test.dll\
+   	-define:DEBUG

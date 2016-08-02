9a4bea51dcb2f4020125b690c2d3a0f13fb1bcb4
amiralles <amiralles.net@gmail.com>
2016-08-02 10:21:03 -0300

    update reduce tests

--- src/test/ReduceFixture.cs
+++ src/test/ReduceFixture.cs
@@ -19,43 +19,42 @@ namespace Abacus.Test {
 		//TODO: Add hook for client func calls.
 		_ reduce_data_table = assert => {
 			var tbl = new DataTable();
-			tbl.Columns.Add("Precio",  typeof(double));
-			tbl.Columns.Add("Fecha",   typeof(DateTime));
-			tbl.Columns.Add("Usuario", typeof(string));
+			tbl.Columns.Add("Price", typeof(double));
+			tbl.Columns.Add("Date",  typeof(DateTime));
+			tbl.Columns.Add("User",  typeof(string));
 
 			var startDate = new DateTime(2012, 05, 14);
 			for (int i = 0; i < 10; ++i) {
 				var row = tbl.NewRow();
-				row["Precio"]  = i;
-				row["Fecha"]   = startDate.AddDays(i);
-				row["Usuario"] = i % 2 == 0 ?  "amiralles" : "pipex";
+				row["Price"]  = i;
+				row["Date"]   = startDate.AddDays(i);
+				row["User"] = i % 2 == 0 ?  "amiralles" : "pipex";
 
 				tbl.Rows.Add(row);
 			}
 
-			var red = tbl.Reduce("Precio >= 5");
+			var red = tbl.Reduce("Price >= 5");
 			assert.Equal(5, red.Rows.Count);
 
-			red = tbl.Reduce("Precio >= 5 or Precio = 2");
+			red = tbl.Reduce("Price >= 5 or Price = 2");
 			assert.Equal(6, red.Rows.Count);
 
-			red = tbl.Reduce("Fecha = dat('15/05/2012')");
+			red = tbl.Reduce("Date = dat('15/05/2012')");
 			assert.Equal(1, red.Rows.Count);
 
 			// Mix table values with local vars.
-			var names  = new [] {"usr"};
-			var locals = new object[] {"pipex"};
-			var fx = "Usuario = usr and Fecha = dat('15/05/2012')";
+			var names  = new [] { "usr" };
+			var locals = new object[] { "pipex" };
+			var fx = "User = usr and Date = dat('15/05/2012')";
 			red = tbl.Reduce(fx, names, locals);
 			assert.Equal(1, red.Rows.Count);
 
-
 			// Date Add
-			red = tbl.Reduce("Fecha = dat('15/05/2012') + 1");
+			red = tbl.Reduce("Date = dat('15/05/2012') + 1");
 			assert.Equal(1, red.Rows.Count);
 
 			// Date Sub
-			red = tbl.Reduce("Fecha = dat('15/05/2012') - 1");
+			red = tbl.Reduce("Date = dat('15/05/2012') - 1");
 			assert.Equal(1, red.Rows.Count);
 		};
 	}
